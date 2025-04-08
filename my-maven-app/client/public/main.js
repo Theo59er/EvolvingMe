92552ad14472056ff76a22eca6f426b2838fcb98
Theo <146164748+Theo59er@users.noreply.github.com>
2025-04-08 14:58:36 +0000

    copilit will helgfen das es geht

---
+++ my-maven-app/client/public/main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('index.html'); // Ändern Sie dies auf 'index.php', wenn Sie PHP verwenden
+});
